81220d154b3b30afe1144501b3c7733409687808
BlackPantific <blackpantific@gmail.com>
2022-03-29 13:17:22 +0300

    invocation of second ctor of class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,7 @@
 package com.company;
-
-
-import models.Base;
-
 public class Main {
 
     public static void main(String[] args) {
-        System
-        Base base = new Base();
-        Base.Inner inner = base.new Inner("Name", base);
     }
 }
 
---
+++ src/models/User.java
@@ -0,0 +1,12 @@
+package models;
+
+public class User {
+    private String name;
+
+    public User() {
+    }
+
+    public User(String name){
+        this();
+    }
+}
